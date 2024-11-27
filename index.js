88815ba8811927965bded08fbc3fb830754ff091
jeycee <jeyceejeyka635@gmail.com>
2024-11-27 22:45:47 +0300

    Adds scope-lab throw new error

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'Elvis'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+  
+  function changeLeastFavoriteCustomer() {
+    throw new Error("Assignment to constant variable.");
+  }
+
